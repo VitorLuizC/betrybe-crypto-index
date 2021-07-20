a25eecb5d7b04637082dd5393a015c35040fc0b4
Lucas Viana <contato@lucasviana.dev>
2021-07-20 00:24:22 -0300

    ➕ (api): adds dotenv dependency

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,6 +3,7 @@ import { createServer } from 'http';
 import Answer from '@betrybe-crypto-index/answer';
 import { ServiceNotFoundAnswer } from './constants/answers';
 import loginRoute from './routes/login';
+import dotenv from 'dotenv';
 
 async function app(request: IncomingMessage, response: ServerResponse) {
   switch (request.url) {
@@ -23,5 +24,6 @@ const PORT = process.env['PORT'] ? parseInt(process.env['PORT']) : 8000;
 const HOSTNAME = process.env['HOSTNAME'] || '127.0.0.1';
 
 server.listen(PORT, HOSTNAME, () => {
+  dotenv.config();
   console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
 });
