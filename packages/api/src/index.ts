48d7b963120824cb4e8d4406196328a3ac80978c
Lucas Viana <contato@lucasviana.dev>
2021-07-19 21:31:40 -0300

    🚚 (api): moves index.ts to src/

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,8 @@
 import type { ServerResponse, IncomingMessage } from 'http';
 import { createServer } from 'http';
-import Answer from '../answer/types/Answer';
-import { ServiceNotFoundAnswer } from './src/constants/answers';
-import loginRoute from './src/routes/login';
+import Answer from '@betrybe-crypto-index/answer';
+import { ServiceNotFoundAnswer } from './constants/answers';
+import loginRoute from './routes/login';
 
 async function app(request: IncomingMessage, response: ServerResponse) {
   switch (request.url) {
@@ -19,8 +19,8 @@ async function app(request: IncomingMessage, response: ServerResponse) {
 
 const server = createServer(app);
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
-const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+const PORT = process.env['PORT'] ? parseInt(process.env['PORT']) : 8000;
+const HOSTNAME = process.env['HOSTNAME'] || '127.0.0.1';
 
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
